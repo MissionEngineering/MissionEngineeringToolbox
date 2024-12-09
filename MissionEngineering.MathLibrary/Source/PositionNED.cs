9b2bc523143b4b2134d6f0d4c8d8ed7f7a29612e
MissionEngineering <mission.engineering@outlook.com>
2024-12-09 21:05:43 +0000

    SG1: Added MathLibrary project.

---
+++ MissionEngineering.MathLibrary/Source/PositionNED.cs
@@ -0,0 +1,17 @@
+﻿namespace MissionEngineering.MathLibrary;
+
+public record PositionNED
+{
+    public double PositionNorth { get; init; }
+
+    public double PositionEast { get; init; }
+
+    public double PositionDown { get; init; }
+
+    public PositionNED(double positionNorth, double positionEast, double positionDown)
+    {
+        PositionNorth = positionNorth;
+        PositionEast = positionEast;
+        PositionDown = positionDown;
+    }
+}
