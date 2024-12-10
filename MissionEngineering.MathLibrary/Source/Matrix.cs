fce0b3e77a4df4cec5c74b294b9c49e9a1d87a49
MissionEngineering <mission.engineering@outlook.com>
2024-12-10 13:14:45 +0000

    SG1: Added Vector and Matrix classes.

---
+++ MissionEngineering.MathLibrary/Source/Matrix.cs
@@ -0,0 +1,30 @@
+﻿namespace MissionEngineering.MathLibrary;
+
+public class Matrix
+{
+    public int NumberOfRows => Data.GetLength(0);
+
+    public int NumberOfColumns => Data.GetLength(1);
+
+    public double[,] Data { get; set; }
+
+    public Matrix() : this(0, 0)
+    {
+    }
+
+    public Matrix(int numberOfRows, int numberOfColumns)
+    {
+        Data = new double[numberOfRows, numberOfColumns];
+    }
+
+    public Matrix(double[,] data)
+    {
+        Data = data;
+    }
+
+    public double this[int rowIndex, int columnIndex]
+    {
+        get => Data[rowIndex, columnIndex];
+        set => Data[rowIndex, columnIndex] = value;
+    }
+}
---
+++ MissionEngineering.MathLibrary/Source/Vector.cs
@@ -0,0 +1,33 @@
+﻿namespace MissionEngineering.MathLibrary;
+
+public class Vector
+{
+    public int NumberOfElements => Data.Length;
+
+    public double[] Data { get; set; }
+
+    public Vector() : this(0)
+    {
+    }
+
+    public Vector(int numberOfElements)
+    {
+        Data = new double[numberOfElements];
+    }
+
+    public Vector(params double[] data)
+    {
+        Data = data;
+    }
+
+    public Vector(params IEnumerable<double> data)
+    {
+        Data = data.ToArray();
+    }
+
+    public double this[int index]
+    {
+        get => Data[index];
+        set => Data[index] = value;
+    }
+}
